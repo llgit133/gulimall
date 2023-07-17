f70466630564601af413d08e6841907d7770fad1
llgit133 <1685268582@qq.com>
2023-07-17 13:27:36 +0800

    first

---
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/CouponApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.gulimall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CouponApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(CouponApplication.class);
+    }
+}
